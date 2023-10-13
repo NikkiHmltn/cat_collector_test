ded8e852ca1a5e0775e121ad2365fd1f4d3f036b
Nicole Hamilton <andromeda@Nicoles-Air-2.lan>
2023-10-13 14:46:20 -0700

    lesson complete w home index about views templates

--- catcollector_app/views.py
+++ catcollector_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+# from django.http import HttpResponse
+
+cats = [
+  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+]
 
 # Create your views here.
+def home(request):
+    return render(request, 'cats/home.html')
+    # return HttpResponse("hello")
+
+def about(request):
+    return render(request, 'cats/about.html')
+
+def cats_index(request):
+    return render(request, 'cats/index.html', {'cats': cats})
