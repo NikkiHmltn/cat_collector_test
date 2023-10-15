ed86ac49c71ad907db5782e2cf4a588254610e4c
Nicole Hamilton <andromeda@Nicoles-Air-2.lan>
2023-10-14 17:20:49 -0700

    tues model django lesson complete

--- catcollector_app/urls.py
+++ catcollector_app/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("about", views.about, name="about"),
-      # route for cats index
+  path("", views.home, name="home"),
+  path("about", views.about, name="about"),
+  # route for cats index
   path('cats/', views.cats_index, name='index'),
+  path('cats/<int:cat_id>/', views.cats_detail, name="detail"),
 ]
--- catcollector_app/views.py
+++ catcollector_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+from .models import Cat
 
-cats = [
-  {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-  {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
-]
+# cats = [
+#   {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
+#   {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+# ]
 
 # Create your views here.
 def home(request):
@@ -15,4 +16,9 @@ def about(request):
     return render(request, 'cats/about.html')
 
 def cats_index(request):
+    cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
+
+def cats_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
